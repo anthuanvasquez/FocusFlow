ad2662e890714627edfb217c975bd49b1918488d
Anthuan Vasquez <me@anthuanvasquez.net>
2025-05-18 16:30:32 -0400

    Initial Commit

---
+++ FocusFlowApp/FocusFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowAppApp.swift
+//  FocusFlowApp
+//
+//  Created by Anthuan Vasquez on 18/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
